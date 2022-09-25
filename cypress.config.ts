48f300cd60835aa03856c7ab6f3f0fb7e0d455c2
motiveko <rhehdrla@naver.com>
2022-09-26 00:03:24 +0900

    uploading parallel changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,20 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://uitestingplayground.com',
+    projectId: 'mkrumj',
+    env: {
+      demoVar: 'Hello from the Cypress.Config.Ts',
+      demoQA: 'https://demoqa.com',
+      theInternet: 'https://the-internet.herokuapp.com',
+      //https://www.globalsqa.com/angularjs-protractor-practice-site/
+      Angular: 'https://www.globalsqa.com',
+      db: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'cypress_test',
+      },
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
